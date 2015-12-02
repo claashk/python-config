149fea74b194c445db510adfcd94973dfb18095e
Claas H. Köhler <claas.koehler@dlr.de>
2015-12-02 11:25:02 +0100

    Renamed test directory to 'tests' and improved test auto discovery

---
+++ tests/context/decorator/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from unittest import defaultTestLoader, TextTestRunner
+import os
+
+def suite():
+    currentDir= os.path.dirname(__file__)
+    topDir= os.path.dirname( os.path.join(currentDir, "../..") )
+
+    return defaultTestLoader.discover( start_dir=currentDir,
+                                       pattern="*.py",
+                                       top_level_dir=topDir )
+
+if __name__ == '__main__':
+    TextTestRunner(verbosity=2).run( suite() )
---
+++ tests/run_tests.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from unittest import TextTestRunner, defaultTestLoader
+import os
+
+def suite():
+    currentDir= os.path.dirname(__file__)
+    topDir= os.path.dirname(currentDir)
+    return defaultTestLoader.discover( start_dir=currentDir,
+                                       pattern="*.py",
+                                       top_level_dir=topDir )
+
+if __name__ == '__main__':
+    TextTestRunner(verbosity=2).run( suite() )
